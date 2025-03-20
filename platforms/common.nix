34c0363f1f4331409fa22f330d23b73087fc54b3
ruarq <anton@ruarq.co>
2025-03-20 14:54:08 +0100

    moved signal-desktop and alacritty to ruarq/home.nix

--- platforms/common.nix
+++ platforms/common.nix
@@ -2,21 +2,15 @@
   nix.settings.experimental-features = "nix-command flakes";
 
   environment.systemPackages = with pkgs; [
-    alacritty
     neovim
     tmux
     git
     zsh
     python3
     rustup
-    signal-desktop
   ];
 
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
-  ];
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-    "signal-desktop"
   ];
 }
--- users/ruarq/home.nix
+++ users/ruarq/home.nix
@@ -12,5 +12,14 @@
     username = "ruarq";
     homeDirectory = if pkgs.stdenv.isDarwin then "/Users/ruarq" else "/home/ruarq";
     stateVersion = "25.05";
+
+    packages = with pkgs; [
+      alacritty
+      signal-desktop
+    ];
   };
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
+    "signal-desktop"
+  ];
 }
