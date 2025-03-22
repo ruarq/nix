e4279b08d54eb3a7a525575f8ba3c98665fdd5bd
ruarq <anton@ruarq.co>
2025-03-22 13:59:50 +0100

    removed git-credential-libsecret as that doesn't exist

--- platforms/common.nix
+++ platforms/common.nix
@@ -15,7 +15,6 @@
 
       # git & libsecret & co
       (git.override { withLibsecret = true; })
-      git-credential-libsecret
       libsecret
 
       # can't move these to /users/ruarq/home.nix (for now) as that
