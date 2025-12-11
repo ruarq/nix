7fd2a8384b831ca5a1fefe61a607108f31c6c027
ruarq <anton@ruarq.co>
2025-12-11 01:04:26 +0100

    Add catppuccin theme to newsboat and rebind keys vim-like

--- home/ruarq/programs/newsboat/default.nix
+++ home/ruarq/programs/newsboat/default.nix
@@ -1,4 +1,11 @@
-{ ... }: {
+{ lib, pkgs, ... }:
+let
+  catppuccin = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/newsboat/refs/heads/main/themes/dark";
+    hash = "sha256-KZ3PQkgF5Q8deYfVsbFBfPQvihwHKZPR/qjRSs4DpSc=";
+  };
+in
+{
   programs.newsboat = {
     enable = true;
     autoReload = true;
@@ -11,5 +18,20 @@
         url = "https://www.tagesschau.de/infoservices/alle-meldungen-100~rss2.xml";
       }
     ];
+
+    extraConfig = ''
+      unbind-key ENTER
+      unbind-key j
+      unbind-key k
+      unbind-key J
+      unbind-key K
+
+      bind-key j down
+      bind-key k up
+      bind-key l open
+      bind-key h quit
+
+      ${lib.readFile catppuccin}
+    '';
   };
 }
